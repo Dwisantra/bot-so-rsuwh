ad38988b582ed6c0e2855e999ceae8eebfcc5d64
dwisantra <itrswiradadihusada@gmail.com>
2025-10-29 15:18:26 +0700

    update config

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 # config.py
 import pymysql.cursors
 
-# TELEGRAM_BOT_TOKEN = "8295989926:AAHS9tmVGflD6DLYhBzEVHMmZ-orj-lDskc"
-TELEGRAM_BOT_TOKEN = "7711520635:AAFWjLijBP-LuUVU0cFHOt-Mo3Z_6YfxIsY"
+TELEGRAM_BOT_TOKEN = "8295989926:AAHS9tmVGflD6DLYhBzEVHMmZ-orj-lDskc"
 
 DB_CONFIG = {
     "host": "10.10.10.9",
